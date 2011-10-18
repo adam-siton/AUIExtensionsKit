3742ba4b455a141333e7b3447620ca821fa94a0d
Adam Siton <adamsiton@gmail.com>
2011-10-18 11:14:43 +0200

    first commit

---
+++ AUIExtensionsKit/AUIExtensionsKit/UIView+AUIInitialization.h
@@ -0,0 +1,16 @@
+//
+//  UIView+AUIInitialization.h
+//  AUIExtensionsKit
+//
+//  Created by Adam Siton on 10/18/11.
+//  Copyright 2011 Any.do. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIView_AUIInitialization : NSObject {
+    
+}
+
+@end
---
+++ AUIExtensionsKit/AUIExtensionsKit/UIView+AUIInitialization.m
@@ -0,0 +1,14 @@
+//
+//  UIView+AUIInitialization.m
+//  AUIExtensionsKit
+//
+//  Created by Adam Siton on 10/18/11.
+//  Copyright 2011 Any.do. All rights reserved.
+//
+
+#import "UIView+AUIInitialization.h"
+
+
+@implementation UIView_AUIInitialization
+
+@end
